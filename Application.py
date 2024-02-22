ba2ea6421721098f438b264c3016d220996642e9
C1003593 <C1003593@my.shu.ac.uk>
2024-02-22 13:36:23 +0000

    Function options implemented.

---
+++ Application.py
@@ -0,0 +1,27 @@
+
+x = 0
+while x == 0:
+    print("Option 1: ")
+    print("Option 2: ")
+    print("Option 3: ")
+    choice = input("Please choose option 1, 2 or 3: ")
+    if choice == "1":
+        print ("You have chosen option 1")
+        x = 1
+
+
+
+    elif choice == "2":
+        print ("You have chosen option 2")
+        x = 1
+
+
+
+    elif choice == "3":
+        print ("You have chosen option 3")
+        x = 1
+
+
+
+    else:
+        print ("Please choose a valid option")
