17f0bca998d26886d9edd03097aa15c55c4a4dbc
C1003593 <C1003593@my.shu.ac.uk>
2024-04-22 14:10:45 +0000

    Added requirements.txt file and added comments

--- Applications/SentimentAnalysis.py
+++ Applications/SentimentAnalysis.py
@@ -9,18 +9,21 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.dates import DateFormatter
 
+#Api key to search Youtube API
 api_key = "AIzaSyAtD0eBdYNMR1crlbNjCc_exLWfDCafxQY"
 youtube = build("youtube", "v3", developerKey=api_key)
 
+#Checks for the video entered
 Videourl = input("Please enter a video url: ")
 VideourlSplit = Videourl.split("=")
 video_id = VideourlSplit[1]
 
+#2 sample videos
 #https://www.youtube.com/watch?v=reUZRyXxUs4
 #https://www.youtube.com/watch?v=QOCZYRXL0AQ
 
+#This function gets all the Youtube comments
 def get_all_english_video_comments(youtube, **kwargs):
     comments = []
     timestamps = []  
@@ -39,7 +42,7 @@ def get_all_english_video_comments(youtube, **kwargs):
             break
     return comments, timestamps
 
-#Comments are preprocessed
+#The comments are preprocessed, things such as punctuation are removed
 def preprocess_comments(comments):
     preprocessed_comments = []
     for comment in comments:
@@ -47,6 +50,7 @@ def preprocess_comments(comments):
         preprocessed_comments.append(comment)
     return preprocessed_comments
 
+#This gets the metadata about the video
 def get_video_details(youtube, video_id):
     request = youtube.videos().list(
         part="snippet,statistics",
@@ -64,6 +68,7 @@ channel_title = video_details['snippet']['channelTitle']
 published_at = video_details['snippet']['publishedAt']
 
 statistics = video_details.get('statistics', {})
+#This ensures that if one of the stats are not available the program will say
 views = statistics.get('viewCount', 'Not available')
 likes = statistics.get('likeCount', 'Not available')
 dislikes = statistics.get('dislikeCount', 'Not available')
@@ -74,15 +79,15 @@ comments, timestamps = get_all_english_video_comments(youtube, part='snippet', v
 
 preprocessed_comments = preprocess_comments(comments)
 
-
+#This starts the vader analyser
 vader_analyzer = SentimentIntensityAnalyzer()
 vader_sentiments = [vader_analyzer.polarity_scores(comment) for comment in preprocessed_comments]
 
-
+#This starts the afinn analyser
 afinn = Afinn()
 afinn_sentiments = [afinn.score(comment) for comment in preprocessed_comments]
 
-
+#This starts the textblob analyser
 textblob_sentiments = [TextBlob(comment).sentiment.polarity for comment in preprocessed_comments]
 
 
@@ -94,13 +99,13 @@ data = {
     'TextBlob Polarity': textblob_sentiments
 }
 
-
+#This loads the data into a pandas dataframe
 df = pd.DataFrame(data)
 
-
+#This gets the 20 most used words
 tfidf_vectorizer = TfidfVectorizer(max_features=20) 
 
-
+#Term Frequency - Inverse Document Frequency, this decides how important a word is, the score is between 0 and 1
 tfidf_matrix = tfidf_vectorizer.fit_transform(preprocessed_comments)
 
 
@@ -109,17 +114,17 @@ feature_names = tfidf_vectorizer.get_feature_names_out()
 
 mean_tfidf_scores = np.mean(tfidf_matrix, axis=0).A1
 
-
+#This loads the top 20 words into a dataframe along with their mean_tfidf scores
 tfidf_df = pd.DataFrame({'Word': feature_names, 'Mean TF-IDF Score': mean_tfidf_scores})
 
 
 tfidf_df = tfidf_df.sort_values(by='Mean TF-IDF Score', ascending=False)
 
-
+#This prints the top 20 words determined by the highest tfidf scores
 print("Top 20 Words by Mean TF-IDF Score:")
 print(tfidf_df[['Word', 'Mean TF-IDF Score']][:20])
 
-
+#This decides if something is positive, neutral or negative based on the score
 def categorize_sentiment(compound_score):
     if compound_score >= 0.05:
         return 'Positive'
@@ -128,7 +133,7 @@ def categorize_sentiment(compound_score):
     else:
         return 'Neutral'
 
-
+#This makes all 3 analysers analyse the sentiments of the comments
 df['VADER Sentiment'] = df['VADER Compound'].apply(categorize_sentiment)
 df['AFINN Sentiment'] = df['AFINN Score'].apply(categorize_sentiment)
 df['TextBlob Sentiment'] = df['TextBlob Polarity'].apply(categorize_sentiment)
@@ -143,7 +148,7 @@ afinn_sentiment_counts = df['AFINN Sentiment'].value_counts()
 textblob_sentiment_counts = df['TextBlob Sentiment'].value_counts()
 
 
-
+#This plots the negative, neutral and positive sentiment distribution for the comments
 plt.figure(figsize=(8, 5))
 labels = ['Positive', 'Neutral', 'Negative']
 vader_sizes = [vader_sentiment_counts.get(sentiment, 0) for sentiment in labels]
@@ -153,19 +158,19 @@ plt.suptitle(f'Positive and negative comment distribution for video: {video_titl
 
 explode = (0.1, 0, 0)  
 
-# VADER
+#Vader sentiment distribution
 plt.subplot(1, 3, 1)
 plt.pie(vader_sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90, colors=['lightblue', 'lightcoral', 'lightgreen'])
 plt.title('VADER Sentiment Distribution')
 
-# AFINN
+#AFINN sentiment distribution
 plt.subplot(1, 3, 2)
 plt.pie(afinn_sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90, colors=['lightblue', 'lightcoral', 'lightgreen'])
 plt.title('AFINN Sentiment Distribution')
 
-# TextBlob
+#TextBlob sentiment distribution
 plt.subplot(1, 3, 3)
 plt.pie(textblob_sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90, colors=['lightblue', 'lightcoral', 'lightgreen'])
@@ -188,6 +193,7 @@ print(textblob_sentiment_counts)
 
 df = df.iloc[::-1]
 
+#This plots the sentiment polarity over time (How positive/negative/neutral the video comments have been)
 plt.figure(figsize=(12, 6))
 plt.plot(df['Timestamps'], df['VADER Compound'], label='VADER', color='lightblue')
 plt.plot(df['Timestamps'], afinn_sentiments, label='AFINN', color='lightcoral')
@@ -206,7 +212,7 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
-
+#This shows how many of each type of comment there are
 plt.figure(figsize=(10, 6))
 sentiments = ['Positive', 'Neutral', 'Negative']
 vader_counts = [vader_sentiment_counts.get(sentiment, 0) for sentiment in sentiments]
@@ -222,14 +228,14 @@ plt.bar(x, vader_counts, width, label='VADER', align='center')
 plt.bar([i + width for i in x], afinn_counts, width, label='AFINN', align='center')
 plt.bar([i + width * 2 for i in x], textblob_counts, width, label='TextBlob', align='center')
 
-
+#This sets the labels
 plt.xlabel('Sentiment Category')
 plt.ylabel('Number of Comments')
 plt.xticks([i + width for i in x], sentiments)
 plt.title(f'Sentiment Analysis Comparison for Video {video_title}')
 plt.legend()
 
-
+#This plots the Top 20 words by TF-IDF score
 plt.figure(figsize=(12, 6))
 plt.barh(tfidf_df['Word'][:20], tfidf_df['Mean TF-IDF Score'][:20], color='lightblue')
 plt.title('Top 20 Words by Mean TF-IDF Score')
@@ -238,9 +244,11 @@ plt.ylabel('Word')
 plt.gca().invert_yaxis()
 plt.show()
 
+#This Filters the published_at number to not include confusing figures
 published_at = published_at.replace("T", " " )
 published_at = published_at.replace("Z", " " )
 print("\n")
+#This displays the metadata about the video
 print("Video Title:", video_title)
 print("Channel Title:", channel_title)
 print("Published At:", published_at)
@@ -249,6 +257,7 @@ print("Description:", video_description)
 print("\n")
 print("Views:", views)
 print("Likes:", likes)
+#Youtube disabled dislikes but this would work if ever reenabled
 print("Dislikes:", dislikes)
 print("Comment count:", comment_count)
 print("\n")
@@ -260,6 +269,7 @@ likesint = int(likes)
 ViewtoLikeRatio = (likesint/viewsint)*100
 ViewtoLikeRatio = round(ViewtoLikeRatio, 1)
 ViewtoLikeRatio = str(ViewtoLikeRatio)
+#This shows how many likes to views the video has
 print("This video has a like to view ratio of " + ViewtoLikeRatio + ":100 or " + ViewtoLikeRatio + "%")
 print("\n")
 
@@ -271,6 +281,7 @@ CommenttoLikeRatio = round(CommenttoLikeRatio, 2)
 CommenttoLikeRatioFigure = CommenttoLikeRatio*10
 CommenttoLikeRatioFigure = str(CommenttoLikeRatioFigure)
 CommenttoLikeRatio = str(CommenttoLikeRatio)
+#This shows how many comments to views the video has
 print("This video has a comment to view ratio of " + CommenttoLikeRatioFigure + ":1000 or " + CommenttoLikeRatio + "%")
 
 
